ac3d15ee32a84fdf36704edce18cb7ddc1c93c38
Dennis Stengele <dstengele@seibert-media.net>
2025-05-02 12:52:49 +0200

    Update

--- home.nix
+++ home.nix
@@ -16,6 +16,7 @@
     jwt-cli
     ffmpeg
     yt-dlp
+    nixfmt
   ];
 
   programs = {
@@ -23,7 +24,10 @@
     go = {
       enable = true;
       packages = {
-        "github.com/barnardb/cookies" = builtins.fetchGit { url = "https://github.com/barnardb/cookies.git"; rev = "964a94d2885feda7378a5faa275981f2664a0c3c";};
+        "github.com/barnardb/cookies" = builtins.fetchGit {
+          url = "https://github.com/barnardb/cookies.git";
+          rev = "964a94d2885feda7378a5faa275981f2664a0c3c";
+        };
       };
     };
     zsh = {
@@ -34,22 +38,14 @@
         append = true;
         extended = true;
       };
-      historySubstringSearch = {
-        enable = true;
-      };
-      syntaxHighlighting = {
-        enable = true;
-      };
+      historySubstringSearch = { enable = true; };
+      syntaxHighlighting = { enable = true; };
       zplug = {
         enable = true;
-        plugins = [
-          {
-            name = "popstas/zsh-command-time";
-          }
-        ];
+        plugins = [{ name = "popstas/zsh-command-time"; }];
       };
       shellAliases = {
-        is_mac = "[[ $(uname) == \"Darwin\" ]]";
+        is_mac = ''[[ $(uname) == "Darwin" ]]'';
         ll = "ls -lah";
         l = "ls -lAh";
         gs = "git status";
@@ -61,17 +57,33 @@
         du = "du -h";
         duh = "du -h -d";
         tmux = "tmux -u";
-        playspace = "play -nq -c1 synth whitenoise band -n 100 20 band -n 50 20 gain +30 fade h 1 86400 1";
-        playnetwork = "sudo tcpdump -n -w- | play --buffer 10000 -r 8000 -b 8 -c 1 -e signed-integer -t raw - band 2k vol 0.1";
+        playspace =
+          "play -nq -c1 synth whitenoise band -n 100 20 band -n 50 20 gain +30 fade h 1 86400 1";
+        playnetwork =
+          "sudo tcpdump -n -w- | play --buffer 10000 -r 8000 -b 8 -c 1 -e signed-integer -t raw - band 2k vol 0.1";
         dunnet = "emacs -batch -l dunnet";
         ls = "ls -Gh --color=auto";
-        path = "echo $PATH | tr \":\" \"\\n\"";
-        csvview = "column -s \\; -t | vim -c 'set scrollopt=hor | set nowrap | 1split | windo set scrollbind' -";
+        path = ''echo $PATH | tr ":" "\n"'';
+        csvview =
+          "column -s \\; -t | vim -c 'set scrollopt=hor | set nowrap | 1split | windo set scrollbind' -";
 
-        wincbpaste="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Get-Clipboard | sed 's/\\r$//' | head -c -1";
-        wincbcopy="/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command '(\\$input | Out-String -Stream) -join \\\"\\`r\\`n\\\" | Set-Clipboard'";
+        wincbpaste =
+          "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Get-Clipboard | sed 's/\\r$//' | head -c -1";
+        wincbcopy =
+          "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command '(\\$input | Out-String -Stream) -join \\\"\\`r\\`n\\\" | Set-Clipboard'";
 
-        disk_ids="ls -la /dev/disk/by-id/ | tail -n+4 | grep -v part | rev | sort | rev | awk '{ printf \"%s\\t%s\\t%s\\n\", $(NF-2), $(NF-1), $(NF) }' | column -t";
+        disk_ids = ''
+          ls -la /dev/disk/by-id/ | tail -n+4 | grep -v part | rev | sort | rev | awk '{ printf "%s\t%s\t%s\n", $(NF-2), $(NF-1), $(NF) }' | column -t'';
+      };
+      localVariables = {
+        VIRTUAL_ENV_DISABLE_PROMPT = "1";
+        WORKON_HOME = "$HOME/.virtualenv";
+        PROJECT_HOME = "$HOME/Development";
+        ZSH_DISABLE_COMPFIX = "true";
+        COLORTERM = "truecolor";
+        EDITOR = "vim";
+        TAPE = "/dev/st0m";
+        AZURE_IDENTITY_DISABLE_CP1 = "1";
       };
       initContent = builtins.readFile ./shellInit.zsh;
     };
--- shellInit.zsh
+++ shellInit.zsh
@@ -1,3 +1,8 @@
+
+# -------------------------------------------------------------------
+# Functions
+# -------------------------------------------------------------------
+
 function disk_ids() {
     ls -la /dev/disk/by-id/ | tail -n+4 | grep -v part | rev | sort | rev | awk '{ printf "%s\t%s\t%s\n", $(NF-2), $(NF-1), $(NF) }' | column -t
 }
@@ -238,3 +243,81 @@ function get-jsm-user-count() {
     get-active-group-members lig_Atlassian-JiraServiceManagement | wc -l
 }
 
+# -------------------------------------------------------------------
+# Keybindings
+# -------------------------------------------------------------------
+
+bindkey -M vicmd '?' history-incremental-search-backward
+bindkey "^R" history-incremental-search-backward
+
+bindkey "^[." insert-last-word
+
+bindkey "^[3;5~"             delete-char
+bindkey "^A"                 beginning-of-line
+bindkey "^E"                 end-of-line
+bindkey "${terminfo[khome]}" beginning-of-line
+bindkey "${terminfo[kend]}"  end-of-line
+bindkey "^[[H"               beginning-of-line
+bindkey "^[[F"               end-of-line
+
+case "${TERM}" in
+  cons25*|linux) # plain BSD/Linux console
+    bindkey '\e[H'    beginning-of-line   # home
+    bindkey '\e[F'    end-of-line         # end
+    bindkey '\e[5~'   delete-char         # delete
+    bindkey '[D'      emacs-backward-word # esc left
+    bindkey '[C'      emacs-forward-word  # esc right
+    ;;
+  *rxvt*) # rxvt derivatives
+    bindkey '\e[3~'   delete-char         # delete
+    bindkey '\eOc'    forward-word        # ctrl right
+    bindkey '\eOd'    backward-word       # ctrl left
+    # workaround for screen + urxvt
+    bindkey '\e[7~'   beginning-of-line   # home
+    bindkey '\e[8~'   end-of-line         # end
+    bindkey '^[[1~'   beginning-of-line   # home
+    bindkey '^[[4~'   end-of-line         # end
+    ;;
+  *xterm*) # xterm derivatives
+    bindkey '\e[H'    beginning-of-line   # home
+    bindkey '\e[F'    end-of-line         # end
+    bindkey '\e[3~'   delete-char         # delete
+    bindkey '\e[1;5C' forward-word        # ctrl right
+    bindkey '\e[1;5D' backward-word       # ctrl left
+    # workaround for screen + xterm
+    bindkey '\e[1~'   beginning-of-line   # home
+    bindkey '\e[4~'   end-of-line         # end
+    ;;
+  screen)
+    bindkey '^[[1~'   beginning-of-line   # home
+    bindkey '^[[4~'   end-of-line         # end
+    bindkey '\e[3~'   delete-char         # delete
+    bindkey '\eOc'    forward-word        # ctrl right
+    bindkey '\eOd'    backward-word       # ctrl left
+    bindkey '^[[1;5C' forward-word        # ctrl right
+    bindkey '^[[1;5D' backward-word       # ctrl left
+    ;;
+esac
+
+
+# Load Bash Completions
+autoload -U +X bashcompinit
+bashcompinit
+if [ -d /etc/bash_completion.d ]; then
+    for bashcompletion in /etc/bash_completion.d/*; do
+        source $bashcompletion
+    done
+fi
+
+zstyle ':completion:*' menu select
+zstyle ':completion::complete:*' gain-privileges 1
+setopt COMPLETE_ALIASES
+
+# Make sshrc use ssh completion
+compdef sshrc=ssh
+
+if command -v jira > /dev/null 2>&1; then
+    source <(jira completion zsh)
+fi
+
+source ./zshTheme.zsh
